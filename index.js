05164217999660d1263110ee7b951f91a67e8752
adapateja <126445884+adapateja@users.noreply.github.com>
2024-08-16 20:35:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ if (process.env.NODE_ENV === 'PRODUCTION') {
     app.use(express.static(path.join(__dirname, '/client/build')))
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '/client/build/index.html'))
+        res.sendFile(path.resolve(__dirname, '../client/build/index.html'))
     })
 }
 
