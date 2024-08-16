500773fe8e56e7fb31b1f6e10428b3ffc0f50bc6
adapateja <126445884+adapateja@users.noreply.github.com>
2024-08-16 20:27:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ const port = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === 'PRODUCTION') {
 
-    app.use(express.static(path.join(__dirname, '../client/build')))
+    app.use(express.static(path.join(__dirname, '/client/build')))
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../client/build/index.html'))
+        res.sendFile(path.resolve(__dirname, '/client/build/index.html'))
     })
 }
 
